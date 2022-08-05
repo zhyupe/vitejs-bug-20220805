f9eeaac870791ed483808b94bf4822fd55e1790a
zhyupe <zyp108421@gmail.com>
2022-08-05 14:29:14 +0800

    add test code

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: []
+    },
+    rollupOptions: {
+      // this would throw error on build
+      external: ['lodash'],
+
+      // this is fine
+      // external: [
+      //   'lodash', 
+      //   'lodash/debounce', 
+      //   'lodash/throttle',
+      //   'lodash/isEqual',
+      // ]
+    },
+  },
+  // enable esbuild pre-compile on build
+  optimizeDeps: {
+    disabled: false
+  }
 })
